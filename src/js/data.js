26574cc07d7b3a69410699df23236e0550bccda2
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-24 07:44:26 +0200

    Add more data

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,22 @@
 export default (function () {
   const data = {
-    ROLLEN1: "rollen",
-    RIEMEN1: "riemen",
+    ROLLEN_1: "rollen",
+    RIEMEN_1: "riemen",
+
+    /** 17m, 1700cm */
+    ROLLEN_BAHN_LENGTH: 1700,
+
+    /** 1.5m, 150cm -- 6 Rollen nicht sichtbar von oben (Eingang und Ausgang) */
+    TROCKER_LENGTH_1: 150,
+
+    /** Make a cut here */
+    TROCKER_LENGTH_2: -1,
+
+    /** 1.6m, 160cm */
+    TROCKNER_AUSGANG_LENGTH: 160,
+
+    /** Keep it simple for now */
+    GLASUR_RIEMEN: 4,
   };
 
   data.rollenBahn = [
@@ -9,63 +24,64 @@ export default (function () {
       name: "Pressen Übergangs Tisch",
       engine: {
         count: 16,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Wender",
       engine: {
         count: 26,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Rollen Bahn 1",
       engine: {
         count: 22,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Rollen Bahn 2",
       engine: {
         count: 22,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Rollen Bahn 3",
       engine: {
         count: 22,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Rollen Bahn 4",
       engine: {
         count: 22,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Rollen Bahn 5",
       engine: {
         count: 14,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Ausricter Eingang",
       engine: {
         count: 14,
-        type: data.ROLLEN1,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Trockner Eingang",
       engine: {
-        count: 15, // NOTE: 2 rollen form TE sind speciell
-        type: data.ROLLEN1,
+        /** 2 Rollen for dem Trocker Eingang sind speziell */
+        count: 15,
+        type: data.ROLLEN_1,
       },
     },
   ];
@@ -74,34 +90,36 @@ export default (function () {
     {
       name: "Gestell Rollen1",
       engine: {
-        count: 13, // NOTE: format 60x60
-        type: data.ROLLEN1,
+        /** Format 60x60 -- Der Antriebsriemen ist ca. 1 Rollen lang */
+        count: 13,
+        type: data.ROLLEN_1,
       },
     },
     {
       name: "Gestell Rollen 2",
       engine: {
-        count: 13, // NOTE: format 60x60
-        type: data.ROLLEN1,
+        /** Format 60x60 -- Der Antriebsriemen ist ca. 11 Rollen lang */
+        count: 9,
+        type: data.ROLLEN_1,
       },
     },
   ];
 
   // Trocknerausgang Riemen
-  data.tagr = {
+  data.trockerAusgangRiemen = {
     name: "Gestell Riemen",
     engine: {
-      count: 6, // NOTE: 6 riemen
-      type: data.RIEMEN1,
+      count: 6,
+      type: data.RIEMEN_1,
     },
   };
 
-  // Proben Tish (P4)
-  data.pt = {
+  // Proben Tisch (P4)
+  data.pressenTisch = {
     name: "Proben Tisch",
     engine: {
-      count: 6, // NOTE: 6 riemen
-      type: data.RIEMEN1,
+      count: 6,
+      type: data.RIEMEN_1,
     },
   };
 
