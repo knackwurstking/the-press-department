005b382680af93fc19c688ff62cd5e9a60ae4b6f
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-25 04:17:40 +0200

    update canvas stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,34 @@
 <script>
+  import { onMount, afterUpdate } from "svelte";
+
   import Data from "./js/data";
+
+
+  /** @type {HTMLCanvasElement} */
+  let canvas
+
+  onMount(() => {
+    console.log("on mount", canvas)
+    // TODO: get the game running
+  })
 </script>
 
-<canvas></canvas>
+<main>
+  <canvas bind:this={canvas}></canvas>
+</main>
 
 <style>
+  main {
+width: 100vw;
+    height: 100vh;
+    display: flex;
+    place-items: center;
+    justify-content: center;
+  }
+
+  canvas {
+    border: var(--border);
+    width: 1700px;
+    height: 150px;
+  }
 </style>
