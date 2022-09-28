ee3c382c5f51543330f1c0030ac1908c45a5ec74
Udo Bauer <knackwurstking.tux@gmail.com>
2022-09-28 02:47:33 +0200

    use pointer events instead of mouse and touch events

--- src/js/game.js
+++ src/js/game.js
@@ -34,80 +34,49 @@ export default class Game {
     this.engines;
 
     // touch event handlers
-    this.touchmove = false;
+    this.pointer = false;
 
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this.ontouchstart = null;
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this._touchstart = () => {
-      //console.log(`[DEBUG] canvas: "touchstart" event`);
-      this.touchmove = true;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this.onpointerdown = null;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this._pointerdown = () => {
+      console.log(`[DEBUG] pointerdown`);
+      this.pointer = true;
     };
 
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this.ontouchmove = null;
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this._touchmove = () => {
-      if (!this.touchmove) return;
-      //console.log(`[DEBUG] canvas: "touchmove" event`);
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this.onpointermove = null;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this._pointermove = () => {
+      if (!this.pointer) return;
+      console.log(`[DEBUG] pointermove`);
     };
 
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this.ontouchend = null;
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this._touchend = () => {
-      //console.log(`[DEBUG] canvas: "touchend" event`);
-      this.touchmove = false;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this.onpointerup = null;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this._pointerup = () => {
+      if (!this.pointer) return;
+      this.pointer = false;
+      console.log(`[DEBUG] pointerup`);
     };
 
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this.ontouchcancel = null;
-    /** @type {null|((ev: TouchEvent) => any)} */
-    this._touchcancel = (ev) => {
-      if (!this.touchmove) return;
-      //console.log(`[DEBUG] canvas: "touchcancel" event`);
-      if (this._touchend) this._touchend(ev);
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this.onpointercancel = null;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this._pointercancel = (ev) => {
+      if (!this.pointer) return;
+      console.log(`[DEBUG] pointercancel`);
+      if (this._pointerup) this._pointerup(ev);
     };
 
-    // mouse event handlers
-    this.mousemove = false;
-
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this.onmousedown = null;
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this._mousedown = () => {
-      //console.log(`[DEBUG] canvas: "mousedown" event`);
-      this.mousemove = true;
-    };
-
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this.onmousemove = null;
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this._mousemove = () => {
-      if (!this.mousemove) return;
-      //console.log(`[DEBUG] canvas: "mousemove" event`);
-    };
-
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this.onmouseup = null;
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this._mouseup = () => {
-      //console.log(`[DEBUG] canvas: "mouseup" event`);
-      this.mousemove = false;
-    };
-
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this.onmouseover = null;
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this._mouseover = null;
-
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this.onmouseout = null;
-    /** @type {null|((ev: MouseEvent) => any)} */
-    this._mouseout = (ev) => {
-      if (!this.mousemove) return;
-      //console.log(`[DEBUG] canvas: "mouseout" event`);
-      if (this._mouseup) this._mouseup(ev);
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this.onpointerout = null;
+    /** @type {null|((ev: PointerEvent) => any)} */
+    this._pointerout = (ev) => {
+      if (!this.pointer) return;
+      console.log(`[DEBUG] pointerout`);
+      if (this._pointerup) this._pointerup(ev);
     };
   }
 
@@ -144,17 +113,11 @@ export default class Game {
   }
 
   handleUserInput() {
-    // handle touch events
-    this.canvas.ontouchstart = this._touchstart;
-    this.canvas.ontouchmove = this._touchmove;
-    this.canvas.ontouchend = this._touchend;
-    this.canvas.ontouchcancel = this._touchcancel;
-    // handle mouse events
-    this.canvas.onmousedown = this._mousedown;
-    this.canvas.onmousemove = this._mousemove;
-    this.canvas.onmouseup = this._mouseup;
-    this.canvas.onmouseover = this._mouseover;
-    this.canvas.onmouseout = this._mouseout;
+    this.canvas.onpointerdown = this._pointerdown;
+    this.canvas.onpointermove = this._pointermove;
+    this.canvas.onpointerup = this._pointerup;
+    this.canvas.onpointercancel = this._pointercancel;
+    this.canvas.onpointerout = this._pointerout;
   }
 
   /** @param {number} hz */
