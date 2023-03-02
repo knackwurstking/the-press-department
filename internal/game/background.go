af3962a1edbb406f55da6f9b157c72b25d8795a6
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-02 03:05:08 +0100

    Adding background doc and todo

--- internal/game/background.go
+++ internal/game/background.go
@@ -1,5 +1,8 @@
 package game
 
+// TODO: for now just fill the background with grey (*Game.Draw method?)
+
+// Background for the game (just some shit with grey)
 type Background struct{}
 
 func NewBackground() *Background {
