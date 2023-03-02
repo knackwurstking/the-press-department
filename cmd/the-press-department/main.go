d93ec28bbec00e9139a19a00cceaf3f460db3e4b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-02 02:35:21 +0100

    Move main to cmd; set initial window size

--- cmd/the-press-department/main.go
+++ cmd/the-press-department/main.go
@@ -22,6 +22,7 @@ func (*Game) Update(screen *ebiten.Image) (err error) {
 }
 
 func main() {
+	ebiten.SetWindowSize(940, 470)
 	ebiten.SetWindowResizable(true)
 	ebiten.SetWindowTitle("The Press Department")
 
