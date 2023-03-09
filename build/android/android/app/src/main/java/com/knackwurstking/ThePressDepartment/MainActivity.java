8168e71f35a1d365fec49815e158d698a4d9859e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-09 04:54:27 +0100

    update project structure

--- build/android/android/app/src/main/java/com/knackwurstking/ThePressDepartment/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.knackwurstking.ThePressDepartment;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ svelte/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.knackwurstking.ThePressDepartment',
+  appName: 'the-press-department',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
