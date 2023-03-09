190b1561a45a38da130eb20a3d4635b36c39c2b6
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-09 05:01:09 +0100

    add wasm stuff to svelte app

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+  // go wasm init
+  if (!WebAssembly.instantiateStreaming) {
+    WebAssembly.instantiateStreaming = async (resp, importObject) => {
+      const source = await (await resp).arrayBuffer();
+      return await WebAssembly.instantiate(source, importObject);
+    };
+  }
+
+  //@ts-expect-error
+  const go = new Go();
+  WebAssembly.instantiateStreaming(
+    fetch("the-press-department.wasm"),
+    go.importObject
+  ).then(result => {
+    go.run(result.instance);
+  });
 </script>
-
-<main>
-</main>
-
-<style>
-</style>
