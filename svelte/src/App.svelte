b0c8d0ff0c1158eeb269c9ae519716a79bf51cc8
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-09 05:07:01 +0100

    update wasm load (iframe) - Fix: unsupported mime type "text/html"

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
-  // go wasm init
-  if (!WebAssembly.instantiateStreaming) {
-    WebAssembly.instantiateStreaming = async (resp, importObject) => {
-      const source = await (await resp).arrayBuffer();
-      return await WebAssembly.instantiate(source, importObject);
-    };
-  }
-
-  //@ts-expect-error
-  const go = new Go();
-  WebAssembly.instantiateStreaming(
-    fetch("the-press-department.wasm"),
-    go.importObject
-  ).then(result => {
-    go.run(result.instance);
-  });
 </script>
+
+<iframe src="main.html" title="The Press Department"></iframe>
+
+<style>
+  iframe {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
