9e92b635ae069219ccb0bf8cd493a6526b1455b3
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-14 17:16:04 +0100

    Adding notes

--- internal/game/sprite.go
+++ internal/game/sprite.go
@@ -2,6 +2,10 @@ package game
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+// NOTE:
+// 	- sprite used for `Roll` (Conveyor)
+// 	- maybe for tiles? (Engines)
+
 type Sprite interface {
 	Draw(screen *ebiten.Image, x, y float64)
 	GetHeight() float64
