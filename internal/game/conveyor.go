6b213da492dc24874dbeecf91e9cc761a42fb00a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-14 16:57:34 +0100

    conveyor draw method

--- internal/game/conveyor.go
+++ internal/game/conveyor.go
@@ -2,28 +2,49 @@ package game
 
 import "github.com/hajimehoshi/ebiten/v2"
 
-type Conveyor struct {
-	rollTypes [3]Roll
-	rolls     []Roll
-	scale     *float64
+type Coord struct {
+	x, y float64
 }
 
-func NewConveyor(scale *float64) Conveyor {
+type Conveyor struct {
+	rollTypes [3]Roll
+	rolls     []Coord
+	scale     *float64
+
+	x, y, width, height float64
+}
+
+func NewConveyor(scale *float64, x, y, width, height float64) Conveyor {
 	return Conveyor{
 		rollTypes: [3]Roll{
 			NewRoll(scale, ImageRoll0),
 			NewRoll(scale, ImageRoll1),
 			NewRoll(scale, ImageRoll2),
 		},
-		rolls: make([]Roll, 0),
-		scale: scale,
+		rolls:  make([]Coord, 0),
+		scale:  scale,
+		x:      x,
+		y:      y,
+		width:  width,
+		height: height,
 	}
 }
 
-func (c *Conveyor) Draw(screen *ebiten.Image, x, y, width, height float64) {
-	// TODO: draw the rollers along the x axis
+func (c *Conveyor) Draw(screen *ebiten.Image) {
+	sprite := c.getSprite()
+	for i := 0; i < len(c.rolls); i++ {
+		sprite.Draw(screen, c.rolls[i].x, c.rolls[i].y)
+	}
+}
+
+func (c *Conveyor) Update() {
+	// TODO: fill c.rolls with rolls
 }
 
 func (c *Conveyor) GetHeight() float64 {
 	return c.rollTypes[0].GetHeight()
+}
+
+func (c *Conveyor) getSprite() Roll {
+	return c.rollTypes[0]
 }
