db001a34fd45736c8313cad9b91c4a34c221e21e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-14 15:10:07 +0100

    Update todos

--- internal/game/conveyor.go
+++ internal/game/conveyor.go
@@ -14,8 +14,10 @@ func NewConveyor(scale *float64) Conveyor {
 }
 
 func (c *Conveyor) Draw(screen *ebiten.Image, x, y, width, height float64) {
+	// TODO: draw the rollers along the x axis
 }
 
 func (c *Conveyor) GetHeight() float64 {
+	// TODO: get height from image assets (only rollers for now)
 	return 0
 }
